96111e597f1cefab6aa87b9f61d3f5ad900ff333
77Kimi77 <155003806+77Kimi77@users.noreply.github.com>
2024-07-08 19:13:27 +0300

    Update Hangman Game.cpp
    Добавлена функция SplitIntoWords, а также функция чтения из файла

--- Hangman Game.cpp
+++ Hangman Game.cpp
@@ -1,28 +1,45 @@
-﻿// Hangman Game.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
 
 
 using namespace std;
-
-int MAX_ATTEMPS = 6;
 
 class Hangman {
 
 public:
 
-	Hangman() {
-		srand(time(NULL));
-		secret_word_ = GetRandomWord();
-		current_word_ = string(secret_word_.length(), '_');
-	}
+    Hangman() {
+        srand(time(NULL));
+        secret_word_ = GetRandomWord();
+        current_word_ = string(secret_word_.length(), '_');
+    }
+    
+    vector<string> SplitIntoWords(const string& text) {
+        vector<string> words;
+        string word;
+        for (const char c : text) {
+            if (c == ' ') {
+                if (!word.empty()) {
+                    words.push_back(word);
+                    word.clear();
+                }
+            } else {
+                word += c;
+            }
+        }
+        if (!word.empty()) {
+            words.push_back(word);
+        }
 
-	void Play() {
-		while (MAX_ATTEMPS > 0) {
+        return words;
+    }
+
+
+    void Play() {
+        while (MAX_ATTEMPS > 0) {
             DisplayGameInfo();
             char guess_letter;
             cout << "Guess the letter: "s;
@@ -46,50 +63,67 @@ public:
                     DisplayGame();
                 }
             }
-		}
+        }
         cout << "You ran out of attemps..."s << endl;
         cout << "Secret word was: "s << secret_word_ << endl;
-	}
+    }
+    vector<string> ReadText(){
+      string line="";
+      vector<string> secret_words;
+      vector<string> secret_words_temp;
+      ifstream file(R"(/Users/rodion/Desktop/proga DZ/Hangman_game/words.txt)");
+      if(file.is_open()){
+          cout<<"file is open"<<endl;
+          getline(file,line);
+          secret_words_temp = (SplitIntoWords(line));
+            for(string& word : secret_words_temp){
+                secret_words.push_back(word);
+            }
+      }
+      return secret_words;
+    }
 
 private:
 
-	string secret_word_;
-	string current_word_;
-	vector<char> guessed_letters_;
-	vector<string> words = { "apple"s,"banana"s,"orange"s,"math"s,"car"s,"cat"s,"dog"s,"horse"s};
+    int MAX_ATTEMPS = 6;
+    string secret_word_;
+    string current_word_;
+    vector<char> guessed_letters_;
+    vector<string> words = ReadText();
 
-	string GetRandomWord() {
-		int index_of_word = rand() % words.size();
-		return words[index_of_word];
-	}
+    string GetRandomWord() {
+        int index_of_word = rand() % words.size();
+        return words[index_of_word];
+    }
 
-	bool LetterAlreadyGuessed(char letter) {
-		return find(guessed_letters_.begin(), guessed_letters_.end(), letter) != guessed_letters_.end();
-	}
+    bool LetterAlreadyGuessed(char letter) {
+        return find(guessed_letters_.begin(), guessed_letters_.end(), letter) != guessed_letters_.end();
+    }
 
-	bool UpdateCurrentWord(char letter) {
-		bool correct_letter = false;
+    bool UpdateCurrentWord(char letter) {
+        bool correct_letter = false;
         for (int i = 0; i < secret_word_.size(); ++i) {
-			if (secret_word_[i] == letter) {
-				correct_letter = true;
+            if (secret_word_[i] == letter) {
+                correct_letter = true;
                 current_word_[i] = letter;
-			}
-		}
-		guessed_letters_.push_back(letter);
-		return correct_letter;
-	}
+            }
+        }
+        guessed_letters_.push_back(letter);
+        return correct_letter;
+    }
+    
 
-	void DisplayGameInfo() {
-		cout << "Word: "s << current_word_ << endl;
-		cout << "You have "s << MAX_ATTEMPS << " attemps left."s << endl;
-		cout << "Guessed letters: "s;
-		for (char& ch : guessed_letters_) {
-			cout << ch << " "s;
-		}
-		cout << endl;
-	}
+    void DisplayGameInfo() {
+        cout << "Word: "s << current_word_ << endl;
+        cout << "You have "s << MAX_ATTEMPS << " attemps left."s << endl;
+        cout << "Guessed letters: "s;
+        for (char& ch : guessed_letters_) {
+            cout << ch << " "s;
+        }
+        cout << endl;
+    }
 
-	void DisplayGame() {
+    void DisplayGame() {
         if (MAX_ATTEMPS == 5) {
             cout << "   _____" << endl;
             cout << "  |     |" << endl;
@@ -144,7 +178,7 @@ private:
             cout << "  |" << endl;
             cout << "  |" << endl;
         }
-	}
+    }
 
 };
 
@@ -154,4 +188,3 @@ int main()
     game.Play();
    
 }
-
